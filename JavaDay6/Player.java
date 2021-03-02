56be80a734abfb7dd3b25ff09832fae121428889
B Pankaj Kumar <bpankaj.kumar@ltts.com>
2021-03-02 17:48:27 +0530

    Java Activities

--- JavaDay6/Player.java
+++ JavaDay6/Player.java
@@ -1,28 +1,28 @@
 public class Player {
-String name;
-String skill;
+	private String name;
+	private String team;
+	private String skill;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTeam() {
+		return team;
+	}
+	public void setTeam(String team) {
+		this.team = team;
+	}
+	public String getSkill() {
+		return skill;
+	}
+	public void setSkill(String skill) {
+		this.skill = skill;
+	}
+	 public String toString(){
+	        return  this.getName() + "--" + this.getTeam() +  "--" + this.getSkill() ;
+	    }
+	
 
-
-
-public Player(String name, String skill) {
-	super();
-	this.name = name;
-	this.skill = skill;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public String getSkill() {
-	return skill;
-}
-public void setSkill(String skill) {
-	this.skill = skill;
-}
-@Override
-public String toString() {
-	return "name=" + name + ", skill=" + skill ;
-}
 }
--- JavaDay6/PlayerMain.java
+++ JavaDay6/PlayerMain.java
@@ -1,46 +1,43 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.TreeMap;
 
+public class Playermain {
 
-public class PlayerMain {
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		ArrayList<Player> al=new ArrayList<Player>();
-		
-	System.out.println("Please provide the number of players to be registered");
-	int n=sc.nextInt();
-	for(int i=0;i<n;i++)
-	{
-		System.out.println("Enter the name of the player ");
-		String name=sc.next();
-		System.out.println("Enter the skill of the player ");
-		System.out.println("1.BATTING");
-		System.out.println("2.BOWLING");
-		System.out.println("3.ALLROUNDER");
-		String skill=sc.next();
-		if(skill.equals("1"))
-		{
-			skill="Batting";
-		}
-		else if(skill.equals("2"))
-		{
-			skill="Bowling";
-		}
-		else if(skill.equals("3"))
-		{
-			skill="Allrounder";
-		}
-		
-		
-		al.add(new Player(name,skill));
+	public static void main(String[] args)throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		 TreeMap<Integer,Player> map=new TreeMap<Integer,Player>();    
+		 BufferedReader reader =new BufferedReader(new InputStreamReader(System.in));
+		 System.out.println("Enter the number of players");
+		 int n = Integer.parseInt(reader.readLine());
+		 int c=1;
+		 for(int i=0;i<n;i++)
+		 {
+			 System.out.println("Enter the details of the player "+c);
+			 c++;
+			 int capNumber = Integer.parseInt(reader.readLine());
+			 System.out.println("Enter the name ");
+			 String name = reader.readLine();
+			 System.out.println("Enter the team ");
+			 String Team = reader.readLine();
+			 System.out.println("Enter the skill ");
+			 String Skill = reader.readLine();
+			 Player p=new Player();
+			 p.setName(name);
+			 p.setTeam(Team);
+			 p.setSkill(Skill);
+			 map.put(capNumber, p);
+			 
+		 }
+		 System.out.println("Player Details");
+		 Set<Map.Entry<Integer, Player>> entries = map.entrySet();
+		 for(Map.Entry<Integer, Player> entry : entries){
+	            System.out.println( entry.getKey() + "--" + entry.getValue() );
+	        }
 	}
-	Collections.sort(al,new PlayerComparator());  
-	for(Player s:al)
-	{
-		System.out.println(s);
-	}
-	}
+
 }
-
